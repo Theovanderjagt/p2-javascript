87be3fb5642ddf1a59cc8801b5359c3953105153
Theovanderjagt <112871814+Theovanderjagt@users.noreply.github.com>
2022-11-24 13:13:23 +0100

    first commit

---
+++ 1.1_p2/opdracht_1.1/opdracht_1.1/app.js
@@ -0,0 +1,32 @@
+let winkelmand =["bananen", "Wisky", "chips","bonen","aardappelen"];
+
+let paragraaf = document.getElementById("para");
+
+//paragraaf.innerText = "er zitten " + winkelmand.length + " artikelen in mijn winkelmand";
+
+//paragraaf.innerText = winkelmand;
+
+//paragraaf.innerText = winkelmand[3] + " staat op de vierde plek in uw winkelmand";
+
+// winkelmand[1] ="bier";
+ //paragraaf.innerText = winkelmand;
+
+ //function product_toevoegen() {
+   // winkelmand.push("bryan");
+   // paragraaf.innerText = winkelmand;
+
+  // if (winkelmand.length <=1){
+   // paragraaf.innerText = "u heeft niet genoeg producten om te tonen"
+  // } else {
+   // paragraaf.innerText = winkelmand;
+  // }
+
+    if (winkelmand.length <=4){
+    paragraaf.innerText = "Winkelmand"
+   } else {
+    paragraaf.innerText = "u heeft geen drop";
+   }
+
+
+
+
---
+++ 1.1_p2/opdracht_1.1/opdracht_1.1/opdracht2/app.js
@@ -0,0 +1,8 @@
+let colors = ["green" , "red" , "blue"];
+
+function changeColors() {
+    let randomColors = Math.floor( Math.random() * 3 );
+    document.body.style.backgroundColor = colors[randomColors];
+}
+
+setInterval(changeColors, 1000)
