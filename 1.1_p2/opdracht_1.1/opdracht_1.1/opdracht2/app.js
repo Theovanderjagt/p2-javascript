5b6a2f2ab74884165eafd13a7fa0b4eb18133fbe
Theovanderjagt <112871814+Theovanderjagt@users.noreply.github.com>
2023-01-12 13:54:05 +0100

    1.2

--- 1.1_p2/opdracht_1.1/opdracht_1.1/opdracht2/app.js
+++ 1.1_p2/opdracht_1.1/opdracht_1.1/opdracht2/app.js
@@ -1,8 +1,23 @@
-let colors = ["green" , "red" , "blue" ]
+//let colors = ["green" , "red" , "blue" ]
 
-function changeColors() {
-    let randomColors = Math.floor( Math.random() * 3 );
-    document.body.style.backgroundColor = colors[randomColors];
+//function changeColors() {
+    //let randomColors = Math.floor( Math.random() * 3 );
+   // document.body.style.backgroundColor = colors[randomColors];
+//}
+
+//setInterval(changeColors, 1000)
+
+let output = document.getElementById("para");
+let input = document.getElementById("input")
+
+let shop = [];
+
+function addproduct(){
+let product = input.value;
+console.log(product);
+
+shop.push(product)
+
+output.innerText = shop;
 }
 
-setInterval(changeColors, 1000)
