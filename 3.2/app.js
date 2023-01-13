d84c53af6cde358725afc42118603f2146a185db
Theovanderjagt <112871814+Theovanderjagt@users.noreply.github.com>
2023-01-13 09:49:05 +0100

    3.2

---
+++ 3.2/app.js
@@ -0,0 +1,5 @@
+let verlanglijst = ["Nieuwe computer", "Smartphone", "E-reader", "Gaming console", "Fitness tracker"]
+
+for (let i =0; i <= verlanglijst.length; i++){
+    document.write(verlanglijst[i] + "br");
+}
